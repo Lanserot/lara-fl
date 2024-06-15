d46bcef2d96db3eca7a6f457ce64364ba4143314
lanserot <55932230+Lanserot@users.noreply.github.com>
2024-06-15 20:09:11 +0300

    начал делать лк для пользователя

---
+++ app/Http/Controllers/User/Api/UserController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers\User\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Buisness\User\Command\RegistrationUserCommand;
+use Buisness\User\ValueObject\UserVO;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Infrastructure\Interfaces\IUserMapper;
+use Infrastructure\Mapper\User\UserMapper;
+use Tools\HttpStatuses;
+
+/**
+ * @see \Tests\Unit\app\Http\Controllers\User\UserControllerTest
+ */
+class UserController extends Controller
+{
+    private const USER_RULES = [
+        User::FIELD_LOGIN => 'required|string|min:5|max:255',
+        User::FIELD_PASSWORD => 'required|string|min:7',
+        User::FIELD_EMAIL => 'required|email',
+    ];
+
+    public function store(Request $request): JsonResponse
+    {
+        $data = $request->all();
+        $validator = Validator::make($data, self::USER_RULES);
+        if ($validator->fails()) {
+            $errors = $validator->errors();
+            $message = '';
+            foreach ($errors->all() as $error) {
+                $message .= $error . ".";
+            }
+            return response()->json(['message' => $message])->setStatusCode((HttpStatuses::BAD_REQUEST)->value);
+        }
+        if($data[UserVO::KEY_PASSWORD] != $data['password_repeat']){
+            return response()->json(['message' => 'Diff password'])->setStatusCode((HttpStatuses::BAD_REQUEST)->value);
+        }
+        /** @var UserMapper $user_mapper */
+        $user_mapper = app(IUserMapper::class);
+        return (new RegistrationUserCommand(
+            $user_mapper->arrayLoginToVoHash($data))
+        )->execute();
+    }
+}
