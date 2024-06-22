144f1b54929bf33cb2ea329c027c3c2a4fd837f3
lanserot <55932230+Lanserot@users.noreply.github.com>
2024-06-22 15:36:16 +0300

    add readme

--- routes/api.php
+++ routes/api.php
@@ -4,18 +4,6 @@ use App\Http\Controllers\Order\Api\OrderController;
 use App\Http\Controllers\User\Api\UserController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
-//Route::middleware(['is.login'])->group(function () {
 Route::middleware('jwt.auth')->group(function () {
     Route::apiResource('users', UserController::class)->names(
         [
@@ -23,14 +11,12 @@ Route::middleware('jwt.auth')->group(function () {
             'show' => 'user.show',
         ]
     )->only(['update', 'show']);
-
     Route::apiResource('orders', OrderController::class)->names(
         [
             'store' => 'order.create'
         ]
     )->only(['store']);
 });
-
 Route::middleware(['not.login'])->group(function () {
     Route::apiResource('users', UserController::class)->names(['store' => 'user.create',])->only(['show']);
 });
